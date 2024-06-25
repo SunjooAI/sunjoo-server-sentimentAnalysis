dd85d0c04f4bc973e6485987684323f122100edd
SunYerim <yaelim6@pukyong.ac.kr>
2024-06-26 00:54:57 +0900

    feat: #2. AnalysisRepository 추가

--- src/main/java/com/sunjoo/sentimentAnalysis/controller/AnalysisController.java
+++ src/main/java/com/sunjoo/sentimentAnalysis/controller/AnalysisController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 @RestController
-public class AnaltysisController {
+public class AnalysisController {
 
     @GetMapping("/getList")
     public void getList(@RequestHeader("userNo") String userNo) {
