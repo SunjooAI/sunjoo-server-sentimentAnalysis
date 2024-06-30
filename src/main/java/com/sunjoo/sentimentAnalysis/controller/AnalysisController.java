0ed3b0c275e10b7adabb17005b4faffbc7fd64f8
SunYerim <yaelim6@pukyong.ac.kr>
2024-07-01 03:12:35 +0900

    feat: #2. 감정분석 관련 controller 작성

--- src/main/java/com/sunjoo/sentimentAnalysis/controller/AnalysisController.java
+++ src/main/java/com/sunjoo/sentimentAnalysis/controller/AnalysisController.java
@@ -1,14 +1,14 @@
 package com.sunjoo.sentimentAnalysis.controller;
 
-import com.sunjoo.sentimentAnalysis.dto.AnalysisResponse;
+import com.sunjoo.sentimentAnalysis.client.UserClient;
+import com.sunjoo.sentimentAnalysis.dto.*;
 import com.sunjoo.sentimentAnalysis.service.AnalysisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnalysisController {
 
     private final AnalysisService analysisService;
+    private final UserClient userClient;
 
     @GetMapping("/{analysis_id}")
     public ResponseEntity<AnalysisResponse> findAnalysis(@PathVariable(value = "analysis_id") Long analysisId, @RequestHeader("Authorization") String token) {
@@ -23,5 +24,31 @@ public class AnalysisController {
 
         return ResponseEntity.ok(analysis);
     }
+
+    @GetMapping
+    public ResponseEntity<List<AnalysisHistory>> findHistoriesByUserId(@RequestHeader("Authorization") String token) {
+        // userId 조회
+        UserInfoResponse userInfoResponse = userClient.getUserInfo("Bearer" + token);
+        UserDTO user = userInfoResponse.getResult();
+
+        final List<AnalysisHistory> histories = analysisService.findHistoriesByUserId(user.getId());
+
+        return ResponseEntity.ok(histories);
+    }
+
+    @PostMapping("/sources")
+    public ResponseEntity<AnalysisCreatedResponse> createAnalysisSource(@RequestHeader("Authorization") String token,
+                                                                        @RequestBody AnalysisRequest sourceRequest) {
+
+        // userId 조회
+        UserInfoResponse userInfoResponse = userClient.getUserInfo("Bearer" + token);
+        UserDTO user = userInfoResponse.getResult();
+
+        final AnalysisResponse analysisResponse = analysisService.postAnalysis(user.getUserNo(), sourceRequest, token);
+
+        return ResponseEntity.ok(new AnalysisCreatedResponse(analysisResponse.getId()));
+    }
+
+
 }
 
